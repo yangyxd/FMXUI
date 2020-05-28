9ce0c4b7e0671c16c8ab5b44e88d01900fe729b6
KngStr <KngStr@QQ.CoM>
2020-05-28 23:05:27 +0800

    add layout demo

---
+++ demos/layout/Unit1.pas
@@ -0,0 +1,41 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, UI.Standard,
+  UI.Base;
+
+type
+  TForm1 = class(TForm)
+    RelativeLayout1: TRelativeLayout;
+    TextView1: TTextView;
+    View1: TView;
+    View2: TView;
+    View3: TView;
+    View4: TView;
+    View5: TView;
+    View6: TView;
+    View7: TView;
+    View8: TView;
+    View9: TView;
+    View10: TView;
+    View11: TView;
+    TextView2: TTextView;
+    View12: TView;
+    View13: TView;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
