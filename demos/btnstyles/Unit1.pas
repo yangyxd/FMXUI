1e466cdd0f285ce136532a600ffe54f0c30d5da6
yangyxd <yangyxd@126.com>
2025-04-25 18:37:11 +0800

    .

---
+++ demos/btnstyles/Unit1.pas
@@ -0,0 +1,28 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, UI.Base,
+  UI.Standard;
+
+type
+  TForm1 = class(TForm)
+    StyleViewManager1: TStyleViewManager;
+    ButtonView1: TButtonView;
+    ButtonView2: TButtonView;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
